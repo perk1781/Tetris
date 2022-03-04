090c8e8c888f1281b7793c37f385086cc26609c8
Aleksandr <perk1781@gmail.com>
2022-03-04 16:00:57 +0500

    Опускание фигур, удаление собранных строк

--- Tetris/Tetris/Field.cs
+++ Tetris/Tetris/Field.cs
@@ -8,8 +8,8 @@ namespace Tetris
 {
     static class Field
     {
-        private static int _width = 40;
-        public static int _height = 40;
+        private static int _width = 20;
+        public static int _height = 20;
         public static int Width
         {
             get { return _width; }
@@ -39,6 +39,57 @@ namespace Tetris
                 _heap[i] = new bool[Width];
             }
         }
+
+        public static void TryDeleteLines()
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                int counter = 0;
+                for (int i = 0; i < Width; i++)
+                {
+                    if (_heap[j][i])
+                        counter++;
+                }
+                if (counter == Width)
+                {
+                    DeleteLine(j);
+                    Redraw();
+                }
+            }
+        }
+
+        private static void Redraw()
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                for (int i = 0; i < Width; i++)
+                {
+                    if (_heap[j][i])
+                        Drawer.DrawPoint(i, j);
+                    else
+                        Drawer.HidePoint(i, j);
+                }
+            }
+        }
+
+        private static void DeleteLine(int line)
+        {
+            for (int j = line; j >= 0; j--)
+            {
+                for (int i = 0; i < Width; i++)
+                {
+                    if (j == 0)
+                    {
+                        _heap[j][i] = false;
+                    }
+                    else
+                    {
+                        _heap[j][i] = _heap[j-1][i];
+                    }
+                }
+            }
+        }
+
         public static bool CheckStrike(Point p)
         {
             return _heap[p.Y][p.X];
--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,25 +1,34 @@
 ﻿using System;
+using System.Timers;
 
 namespace Tetris
 {
     class Program
     {
+        const int TIMER_INTERVAL = 500;
+        static System.Timers.Timer timer;
+        static private Object _lockObject = new Object();
+
+        static Figure currentFigure;
         static FigureGenerator generator;
         static void Main(string[] args)
         {
             Console.SetWindowSize(Field.Width, Field.Height);
             Console.SetBufferSize(Field.Width, Field.Height);
 
-            generator = new FigureGenerator(20, 0, '*');
-            Figure currentFigure = generator.GetNewFigure();
+            generator = new FigureGenerator(Field.Width / 2, 0, Drawer.DEFAULT_SYMBOL);
+            currentFigure = generator.GetNewFigure();
+            SetTimer();
 
             while (true)
             {
                 if (Console.KeyAvailable)
                 {
                    var key = Console.ReadKey();
+                   Monitor.Enter(_lockObject);
                    var result = HandleKey(currentFigure, key);
                    ProcessResult(result, ref currentFigure);
+                   Monitor.Exit(_lockObject);  
                 }
             }
 
@@ -31,6 +40,7 @@ namespace Tetris
             if (result == Result.HEAP_STRIKE || result == Result.DOWN_BORDER_STRIKE)
             {
                 Field.AddFigure(currentFigure);
+                Field.TryDeleteLines();
                 currentFigure = generator.GetNewFigure();
                 return true;
             }
@@ -52,6 +62,22 @@ namespace Tetris
                     return f.TryRotate();
             }
             return Result.SUCCESS;
+        }
+        
+        private static void SetTimer()
+        {
+            timer = new System.Timers.Timer(TIMER_INTERVAL);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Monitor.Enter(_lockObject);
+            var result = currentFigure.TryMove(Direction.DOWN);
+            ProcessResult(result, ref currentFigure);
+            Monitor.Exit(_lockObject);
         }
     }
 }
